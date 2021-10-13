d49bb1de2cd1df91f41d2c2a08d656478c6bfe2f
Yunus <yunskilic@gmail.com>
2021-10-13 18:48:50 +0300

    refactor

--- go.mod
+++ go.mod
@@ -1,3 +1,4 @@
 module github.com/yunuskilicdev/yemeksepeti
 
+// +heroku goVersion go1.17
 go 1.17
--- keyvalueservice.go
+++
@@ -1,6 +0,0 @@
-package main
-
-type KeyValueService interface {
-	Get(key string) string
-	Put(key string, value string)
-}
